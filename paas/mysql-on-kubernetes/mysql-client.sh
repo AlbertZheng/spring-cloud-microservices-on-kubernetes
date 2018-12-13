aebee2fa75465c2fa30803982073b7208ce8fa3d
Albert Zheng <lisong.zheng@gmail.com>
2018-12-13 12:07:51 +0800

    Update mysql-client.sh

--- paas/mysql-on-kubernetes/mysql-client.sh
+++ paas/mysql-on-kubernetes/mysql-client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl run mysql-client -it --rm --image=mysql:5.7.24 --restart=Never -- mysql -h mysql -pXXXX
+kubectl run mysql-client -it --rm --image=mysql:5.7.24 --restart=Never -- mysql -h mysql -p$1
